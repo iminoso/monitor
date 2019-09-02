d6f28c52a8f3957bb3802f340f23ef4a806dca4a
Ian Minoso <iminoso@gmail.com>
2019-09-02 00:02:41 -0400

    Basic streaming process utilization

--- lib/monitor_web/live/counter_live.ex
+++ lib/monitor_web/live/counter_live.ex
@@ -1,27 +1,52 @@
 defmodule Monitor.CounterLive do
   use Phoenix.LiveView
 
-    def render(assigns) do
+  def render(assigns) do
     ~L"""
+    <svg viewBox="0 0 500 200" class="chart">
+
+    <polyline
+      fill="none"
+      stroke="#0074d9"
+      stroke-width="2"
+      points="<%= @process_data |> convert_data() %>"
+    />
+
+    </svg>
     <div class="">
       <div>
-        <%= @count %>
+        <%= @process_data |> Enum.join(" ") %>
       </div>
     </div>
     """
   end
 
-    def mount(_session, socket) do
+  def mount(_session, socket) do
     tick()
-    {:ok, assign(socket, count: 0)}
+    {:ok, assign(socket, process_data: [0,0,0,0,0,0,0,0,0,0])}
   end
 
-    def handle_info(:tick, socket) do
+  def handle_info(:tick, %{assigns: %{process_data: process_data}} = socket) do
     tick()
-    {:noreply, update(socket, :count, &(&1 + 1))}
+    cpu_util = :cpu_sup.util() |> Kernel.trunc()
+    {:noreply, assign(socket, process_data: insert_data_point(process_data, cpu_util))}
   end
 
-    defp tick() do
-    self() |> Process.send_after(:tick, 500)
+  defp tick() do
+    self() |> Process.send_after(:tick, 1000)
+  end
+
+  # Convert list into string of points accepted by `polyline` element
+  defp convert_data(points) do
+    str_points = Enum.with_index(points) |> Enum.map(fn {y,x} ->
+      "#{x * 20},#{(100 - y)}"
+    end)
+    str_points |> Enum.join(" ")
+  end
+
+  # Add data point to end
+  defp insert_data_point(data, val) do
+    [_ | tail] = data
+    tail ++ [val]
   end
 end
--- mix.exs
+++ mix.exs
@@ -19,7 +19,7 @@ defmodule Monitor.MixProject do
   def application do
     [
       mod: {Monitor.Application, []},
-      extra_applications: [:logger, :runtime_tools]
+      extra_applications: [:logger, :runtime_tools, :os_mon, :sasl]
     ]
   end
 
