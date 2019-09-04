64938a356c1a2c06aa79caa2396bdb9a2a984e9c
Ian Minoso <iminoso@gmail.com>
2019-09-04 00:41:20 -0400

    Heroku config

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,12 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :monitor, MonitorWeb.Endpoint,
-  url: [host: "example.com", port: 80],
+  url: [host: "intense-wildwood-89922", port: 443],
+  check_origin: [
+    "http://intense-wildwood-89922.herokuapp.com",
+    "https://intense-wildwood-89922.herokuapp.com"
+  ],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
