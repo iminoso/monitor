e0578c415b6504fbec25676b6dc6bb6e81482950
Ian Minoso <iminoso@gmail.com>
2019-09-06 03:00:47 -0400

    Chart with lines, taking cpu averages evey 10 seconds

--- lib/monitor_web/live/process_live.ex
+++ lib/monitor_web/live/process_live.ex
@@ -3,15 +3,20 @@ defmodule Monitor.ProcessLive do
 
   def render(assigns) do
     ~L"""
+    <h3>System Process Utilization Percentage</h3>
     <svg viewBox="0 0 600 200" class="chart">
-
-    <polyline
-      fill="none"
-      stroke="#0074d9"
-      stroke-width="2"
-      points="<%= @process_data |> convert_data() %>"
-    />
-
+      <line x1="0" y1="150" x2="600" y2="150" stroke="#555" stroke-width="1" stroke-dasharray="2" />
+      <line x1="0" y1="100" x2="600" y2="100" stroke="#555" stroke-width="1" stroke-dasharray="2" />
+      <line x1="0" y1="50" x2="600" y2="50" stroke="#555" stroke-width="1" stroke-dasharray="2" />
+      <text x="0" y="150" class="chart__label">25</text>
+      <text x="0" y="100" class="chart__label">50</text>
+      <text x="0" y="50" class="chart__label">75</text>
+      <polyline
+        fill="none"
+        stroke="#00749d"
+        stroke-width="2"
+        points="<%= @process_data |> convert_data() %>"
+      />
     </svg>
     <div class="">
       <div>
@@ -27,21 +32,24 @@ defmodule Monitor.ProcessLive do
       :ok,
       assign(
         socket,
-        process_data: List.duplicate(0, 60)
+        process_data: List.duplicate(0, 60),
+        process_window: []
       )
     }
   end
 
-  def handle_info(:tick, %{assigns: %{process_data: process_data}} = socket) do
+  def handle_info(:tick, %{assigns: %{process_data: process_data, process_window: process_window}} = socket) do
     tick()
     cpu_util = :cpu_sup.util() |> Kernel.trunc()
-    {
-      :noreply,
-      assign(
-        socket,
-        process_data: insert_data_point(process_data, cpu_util)
-      )
-    }
+    process_window = process_window ++ [cpu_util]
+
+    if length(process_window) == 10 do
+      process_data = insert_data_point(process_data, Enum.sum(process_window) / 10)
+      process_window = []
+      {:noreply, assign(socket, process_data: process_data, process_window: process_window)}
+    else
+      {:noreply, assign(socket, process_data: process_data, process_window: process_window)}
+    end
   end
 
   defp tick() do
