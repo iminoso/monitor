8fc913df726efb2f940f35229ff24b9ab65e5bf7
Ian Minoso <iminoso@gmail.com>
2019-09-08 14:03:55 -0400

    Log history with reverse printing order

--- lib/monitor/util.ex
+++ lib/monitor/util.ex
@@ -46,7 +46,7 @@ defmodule Monitor.Util do
   # Calculates memory of the system returning a keyword list of the form in MB:
   # eg. [total_memory: _, free_memory: _, system_total_memory: _]
   def system_memory_data() do
-    :memsup.get_system_memory_data
-    |> Keyword.new(fn {k,v} -> {k, (v / 1000000) |> Kernel.trunc()} end)
+    :memsup.get_system_memory_data()
+    |> Keyword.new(fn {k, v} -> {k, (v / 1_000_000) |> Kernel.trunc()} end)
   end
 end
--- lib/monitor_web/live/process_live.ex
+++ lib/monitor_web/live/process_live.ex
@@ -2,9 +2,11 @@ defmodule Monitor.ProcessLive do
   use Phoenix.LiveView
   alias Monitor.Util
 
+  @window_length 10
+
   def render(assigns) do
     ~L"""
-    <h3>System Process Utilization Percentage</h3>
+    <h3 class="header"> Process Utilization Percentage</h3>
     <svg viewBox="0 0 600 200" class="chart">
       <line x1="0" y1="150" x2="600" y2="150" stroke="#555" stroke-width="1" stroke-dasharray="2" />
       <line x1="0" y1="100" x2="600" y2="100" stroke="#555" stroke-width="1" stroke-dasharray="2" />
@@ -16,35 +18,35 @@ defmodule Monitor.ProcessLive do
         fill="none"
         stroke="#00749d"
         stroke-width="2"
-        points="<%= @process_info |> convert_data() %>"
+        points="<%= @system_info |> convert_data() %>"
       />
     </svg>
-
+    <h3 class="header">Log History</h3>
     <table>
       <thead>
         <tr>
-          <th>Timestamp</th>
-          <th>Process Utilization %</th>
+          <th>UTC Timestamp</th>
+          <th>Process Utilization Percentage</th>
           <th>Memory Used</th>
           <th>Memory Available</th>
         </tr>
       </thead>
       <tbody>
       <%= unless @loading_initial_data do %>
-        <%= for {p, index} <- Enum.with_index(@process_info) do  %>
-          <%= if p do %>
+        <%= for s <- @system_info |> Enum.reverse() do  %>
+          <%= if s do %>
             <tr>
               <td>
-                <%= @timestamp |> Enum.at(index) |> Time.to_iso8601() %>
+                <%= s |> Keyword.get(:timestamp) |> Time.to_iso8601() %>
               </td>
               <td>
-                <%= @process_info |> Enum.at(index) %>
+                <%= s |> Keyword.get(:process_average) %>
               </td>
               <td>
-                <%= @memory_info |> Enum.at(index) |> Keyword.get(:used_memory) %> MB
+                <%= s |> Keyword.get(:used_memory) %> MB
               </td>
               <td>
-                <%= @memory_info |> Enum.at(index) |> Keyword.get(:free_memory) %> MB
+                <%= s |> Keyword.get(:free_memory) %> MB
               </td>
             </tr>
           <% end %>
@@ -61,37 +63,45 @@ defmodule Monitor.ProcessLive do
 
   def mount(_session, socket) do
     tick()
+
     {
       :ok,
       assign(
         socket,
-        process_info: List.duplicate(nil, 60),
-        memory_info: List.duplicate(nil, 60),
+        system_info: List.duplicate(nil, 60),
         process_window: [],
-        timestamp: List.duplicate(nil, 60),
         loading_initial_data: true
       )
     }
   end
 
-  def handle_info(:tick, %{assigns: %{process_info: process_info, memory_info: memory_info, timestamp: timestamp, process_window: process_window}} = socket)
-    when length(process_window) == 9
-  do
+  def handle_info(
+        :tick,
+        %{
+          assigns: %{
+            system_info: system_info,
+            process_window: process_window
+          }
+        } = socket
+      )
+      when length(process_window) == @window_length do
     tick()
-    process_window = process_window ++ [Util.cpu_util]
+
     {
       :noreply,
       assign(
         socket,
-        process_info: insert_data_point(process_info, Enum.sum(process_window) / 10),
-        memory_info: insert_data_point(
-          memory_info,
-          Keyword.new(
-            [{:free_memory, Util.free_memory()}, {:used_memory, Util.used_memory()}]
-          )
-        ),
-        process_window: [],
-        timestamp: insert_data_point(timestamp, Time.utc_now() |> Time.truncate(:second)),
+        system_info:
+          insert_data_point(
+            system_info,
+            Keyword.new([
+              {:timestamp, Time.utc_now() |> Time.truncate(:second)},
+              {:process_average, (Enum.sum(process_window) / @window_length) |> Kernel.trunc()},
+              {:free_memory, Util.free_memory()},
+              {:used_memory, Util.used_memory()}
+            ])
+          ),
+        process_window: [Util.cpu_util()],
         loading_initial_data: false
       )
     }
@@ -99,7 +109,7 @@ defmodule Monitor.ProcessLive do
 
   def handle_info(:tick, %{assigns: %{process_window: process_window}} = socket) do
     tick()
-    {:noreply, assign(socket, process_window: process_window ++ [Util.cpu_util])}
+    {:noreply, assign(socket, process_window: process_window ++ [Util.cpu_util()])}
   end
 
   defp tick() do
@@ -107,11 +117,14 @@ defmodule Monitor.ProcessLive do
   end
 
   # Convert list into string of points accepted by `polyline` svg element
-  defp convert_data(points) do
-    str_points = Enum.with_index(points) |> Enum.map(fn {y,x} ->
-      y = if (y == nil), do: 0, else: y
-      "#{x * 10},#{(200 - (y * 2))}"
-    end)
+  defp convert_data(data_points) do
+    str_points =
+      Enum.with_index(data_points)
+      |> Enum.map(fn {data_value, timestamp} ->
+        data_value = if data_value == nil, do: 0, else: Keyword.get(data_value, :process_average)
+        "#{timestamp * 10},#{200 - data_value * 2}"
+      end)
+
     str_points |> Enum.join(" ")
   end
 
